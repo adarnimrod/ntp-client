a783513aa3ce3936d1d67d5e5c55bbca9493b8f4
Adar Nimrod <nimrod@shore.co.il>
2016-07-13 22:31:07 +0300

    - Some more tests.

--- tests/test_ntp.py
+++ tests/test_ntp.py
@@ -1,7 +1,12 @@
 def test_ntp(Service, Ansible):
     if Ansible('setup')['ansible_facts']['ansible_service_mgr'] == 'systemd':
-        assert Service('systemd-timesyncd').is_running
+        service = Service('systemd-timesyncd')
     elif Ansible('setup')['ansible_facts']['ansible_os_family'] == 'Debian':
-        assert Service('openntpd').is_running
+        service = Service('openntpd')
     elif Ansible('setup')['ansible_facts']['ansible_os_family'] == 'OpenBSD':
-        assert Service('ntpd').is_running
+        service = Service('ntpd')
+    assert service.is_running
+    try:
+        assert service.is_enabled
+    except NotImplementedError:
+        pass
