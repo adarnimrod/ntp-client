00ffcb0d230a4fb9f96ae4ad990001d0f4a4feb0
Adar Nimrod <nimrod@shore.co.il>
2016-12-15 19:07:39 +0200

    - Corrected test.

--- tests/test_ntp.py
+++ tests/test_ntp.py
@@ -5,8 +5,9 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 def test_ntp(Service):
     assert Service('systemd-timesyncd').is_running or Service(
-        'ntpd').is_running
+        'ntpd').is_running or Service('openntpd').is_running
     try:
-        Service('systemd-timesyncd').is_enabled or Service('ntpd').is_enabled
+        Service('systemd-timesyncd').is_enabled or Service(
+            'ntpd').is_enabled or Service('openntpd').is_enabled
     except NotImplementedError:
         pass
